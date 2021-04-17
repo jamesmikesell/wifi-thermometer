8e9e5484277963836cc597d4316077f9817bec4e
James Mikesell <james.mikesell@gmail.com>
2021-04-17 00:25:38 -0400

    refactor(ui): moving current temp to it's own component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TimeagoModule } from 'ngx-timeago';
 import { AppComponent } from './app.component';
+import { CurrentTempComponent } from './component/current-temp/current-temp.component';
 import { LoadingSpinnerComponent } from './component/loading-spinner/loading-spinner.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    CurrentTempComponent
   ],
   imports: [
     BrowserModule,
